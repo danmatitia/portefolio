0598160a4be3954a81e4026d58a437c1a6c71d79
matitia <jmatitia@gmail.com>
2022-04-08 15:50:29 +0200

    installation sass

---
+++ src/components/formationCard.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="formation--card">
+
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style lang="scss">
+    .formation--card{
+        width: 20%;
+        height: 30%;
+    }
+</style>
